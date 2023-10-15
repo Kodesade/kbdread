c02b8a53bf2996ab42416423ea796bbd7aba9f49
Luca <64040686+Kodesade@users.noreply.github.com>
2023-10-15 09:46:48 +0200

    Create device_event_id to clarify the code

--- test.rb
+++ test.rb
@@ -4,8 +4,8 @@ log = -> (events) {
   next if events.empty?
   puts events.keys.map{ _1.length == 1 ? _1.downcase : _1.capitalize }.join("+")
 }
-
-keyboard = KeyboardInput.new(10,_evproc: log)
+device_event_id = 10
+keyboard = KeyboardInput.new(device_event_id,_evproc: log)
 
 keyboard.wait(*%w[leftcontrol leftshift q])
 puts "{===== END =====}"
